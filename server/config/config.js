b929bd48f165499d4eaaa3a6eaf1b55a5d598b4c
Fredy Orellana <forellanaf@tigo.com.gt>
2018-08-21 11:17:56 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:welcome1@ds243059.mlab.com:43059/cafe-udemy';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
